36dc06999e16629a301dd6ff8ecc486f915f661f
MatiasTarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2023-09-03 19:53:16 -0300

    clase corredor creada

---
+++ src/Corredor.java
@@ -0,0 +1,21 @@
+class Corredor implements Runnable {
+    private String nombre;
+    private int distanciaRecorrida;
+
+    public Corredor(String nombre) {
+        this.nombre = nombre;
+        this.distanciaRecorrida = 0;
+    }
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(nombre + " ha recorrido " + (i * 10) + " metros.");
+            distanciaRecorrida = i * 10;
+            try {
+                Thread.sleep(100); // Simula el tiempo que toma avanzar
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(nombre + " ha terminado la carrera!");
+    }
+}
