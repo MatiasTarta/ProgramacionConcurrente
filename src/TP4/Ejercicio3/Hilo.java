6d79774da310e80709389d65c1ba6059eaaa58a4
matiastarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2023-09-20 14:02:02 -0300

    Merge branch 'master' of https://github.com/MatiasTarta/ProgramacionConcurrente

---
+++ src/TP4/Ejercicio3/Hilo.java
@@ -0,0 +1,21 @@
+package TP4.Ejercicio3;
+
+public class Hilo extends Thread {
+    private char nombre;
+    private int repeticiones;
+    private RecursoCompartido recurso= new RecursoCompartido();
+
+    public Hilo(char nombreProceso,int repeticiones){
+        this.nombre=nombreProceso;
+        this.repeticiones= repeticiones;
+    }
+    public void run(){
+        try {
+            recurso.imprimir(repeticiones, nombre);
+            System.out.println();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/TP4/Ejercicio3/Main.java
@@ -0,0 +1,14 @@
+package TP4.Ejercicio3;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // Código a ejecutar
+        Hilo P1= new Hilo('1', 3);
+        Hilo P2= new Hilo('2', 3);
+        Hilo P3= new Hilo('3', 3);
+        P1.start();
+        P2.start();
+        P3.start();
+    }
+}
