5e27f6a3576735211f339ffe424faaac0308ebd1
MatiasTarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2023-10-24 09:41:54 -0300

    xdxd

--- src/TP4/Ejercicio5/Main.java
+++ src/TP4/Ejercicio5/Main.java
@@ -2,12 +2,15 @@ package TP4.Ejercicio5;
 
 public class Main {
     public static void main(String[] args){
-        Taxi nave= new Taxi();
+        /*
+         * Taxi nave= new Taxi();
         Taxista arjona = new Taxista(nave);
         Pasajero davor = new Pasajero("Dominik", nave);
         Pasajero membrillo = new Pasajero("Membrillo", nave);
         arjona.start();
         davor.start();
         membrillo.start();
+         */
+        
     }
 }
