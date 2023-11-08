80728748e56053b0ca076ecc75cdab724a96c72a
MatiasTarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2023-11-08 16:48:27 -0300

    a

--- src/TP6/Ejercicio2/Sala.java
+++ src/TP6/Ejercicio2/Sala.java
@@ -3,25 +3,23 @@ package TP6.Ejercicio2;
 import java.util.Random;
 
 public class Sala {
-    private int mesas, mesasDisponibles, numAleatorio;
+    private int  mesasDisponibles;
     Random random = new Random();
 
     public Sala(int cantidadMesas) {
-        mesas = cantidadMesas;
         mesasDisponibles = cantidadMesas;
-        numAleatorio = random.nextInt(10) + 1;
     }
 
     public synchronized void entrar(int id) throws InterruptedException {
         while (mesasDisponibles <= 0) {
-            System.out.println(id+" debe esperar");
             wait();
+            System.out.println(id+" debe esperar");
         }
         // el estudiante ocupa mesa
          System.out.println(id + " se sento");
         mesasDisponibles--;
         try {
-            Thread.sleep(1000);
+            Thread.sleep(random.nextInt(10)*(1000));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -33,6 +31,6 @@ public class Sala {
     public void salir(int id) {
         System.out.println(id + " se fue de la sala");
         mesasDisponibles++;
-        this.notify();
+        this.notifyAll();
     }
 }
