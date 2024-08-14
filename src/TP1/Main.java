428c8dc9c3d8243d2debe1660052818b4451e2c6
MatiasTarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2024-08-13 21:53:42 -0300

    haciendo tp1

--- src/TP1/Main.java
+++ src/TP1/Main.java
@@ -2,7 +2,21 @@ package TP1;
 
 public class Main {
     public static void main(String[] args) {
-        //comentario
-        System.out.println("Hola Mundo");
+        double[] v = new double[15];
+        acceso_por_indice(v, 16);
+
+    }
+
+    public static double acceso_por_indice(double[] v, int j)
+            throws RuntimeException {
+        try {
+            if ((0 <= j) && (j <= v.length)) {
+                return v[j];
+            } else {
+                throw new RuntimeException("El indice " + j + " no existe en el vector");
+            }
+        } catch (RuntimeException exc) {
+            throw exc;
+        }
     }
 }
