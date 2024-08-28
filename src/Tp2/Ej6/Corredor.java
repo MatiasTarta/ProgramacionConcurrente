49e2f851b8b1b664c7f15c497a24fa3e97e0dfa0
matiastarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2024-08-28 18:28:38 -0300

    tp2 ready

--- src/Tp2/Ej6/Corredor.java
+++ src/Tp2/Ej6/Corredor.java
@@ -1,12 +1,16 @@
 package Tp2.Ej6;
 
+import java.util.Random;
+
 public class Corredor implements Runnable {
       private String nombre;
       private int pasos;
+      private Random random;
 
     public Corredor(String nombre) {
         this.nombre = nombre;
         pasos=0;
+         random = new Random();
     }
 
     public String getNombre(){
@@ -16,8 +20,44 @@ public class Corredor implements Runnable {
     public void run(){
        while (pasos<100) {
         int numeroAleatorio = random.nextInt(10) + 1;
-        System.out.println("El corredor" + nombre +" dio "+numeroAleatorio+ " pasos" );
-        pasos+=numeroAleatorio;
+
+        if(pasos+numeroAleatorio<=100){
+            pasos+=numeroAleatorio;
+        }
+        System.out.println("El corredor " + nombre +" dio "+numeroAleatorio+ " pasos.Total Pasos-- "+pasos+" --" );
+        try{
+            Thread.sleep(1000);
+        }catch(InterruptedException e){
+
+        }
+
        }
+       System.out.println(nombre+" Termino la carrera");
+    }
+
+     public static void main(String[] args) {
+        // Crear instancias de corredores
+        Corredor corredor1 = new Corredor("Usain Bolt");
+        Corredor corredor2 = new Corredor("Correcaminos");
+        Corredor corredor3 = new Corredor("Barry Allen");
+
+        // Crear hilos para cada corredor
+        Thread hilo1 = new Thread(corredor1);
+        Thread hilo2 = new Thread(corredor2);
+        Thread hilo3 = new Thread(corredor3);
+
+        // Iniciar los hilos
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+        
+       try{
+        hilo1.join();
+        hilo2.join();
+        hilo3.join();
+       }catch(InterruptedException e){
+        System.out.println();
+       }
+        
     }
 }
