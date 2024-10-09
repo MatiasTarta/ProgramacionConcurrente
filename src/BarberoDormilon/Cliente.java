86ddbbb998015fb291bb4c9fe64ec63318fa0445
matiastarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2024-10-09 19:10:08 -0300

    barbero dormilon con mian

--- src/BarberoDormilon/Cliente.java
+++ src/BarberoDormilon/Cliente.java
@@ -13,13 +13,15 @@ public class Cliente implements Runnable {
     public void run() {
         try {
             eljony.sentarse();// una vez sentado el cliente despierta al barbero
-            System.out.println("Cliente N " + nombre + " pide un buzz cut");
+            System.out.println("Desperta viejo meado");
             eljony.despertarBarbero();
+            // System.out.println("Cliente N " + nombre + " pide un buzz cut");
+            Thread.sleep(3000);
             eljony.pagar();// adquiere semaforo salida para irse a la goma
             System.out.println("Cliente N " + nombre + " se fue al boliche");
             eljony.levantarse();
-        } catch (Exception e) {
-            // TODO: handle exception
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
     }
 
