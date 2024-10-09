4571a55fdbdfcf2f45081df4a6e26d4b30759ea6
matiastarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2024-10-09 19:10:18 -0300

    sleepi barber

--- src/BarberoDormilon/Barbero.java
+++ src/BarberoDormilon/Barbero.java
@@ -12,15 +12,17 @@ public class Barbero implements Runnable {
             // es un while true porque el barbero permanece vivo siempre, no termina su
             // ciclo de vida
             try {
-                eljony.dormir();// duerme hasta que un cliente lo despierta
-                System.out.println("Ya me desperte jefazo te corto");
+                // duerme hasta que un cliente lo despierta
+                eljony.dormir();
                 // si el programa continua es porque un cliente lo desperto
+                System.out.println("Ya me desperte jefazo te corto");
                 Thread.sleep(4000);
                 System.out.println("Quedo flama");
                 eljony.cobrar();// indica la salida al cliente
                 System.out.println("Ahora a dormir una siestonga");
-            } catch (Exception e) {
-                // TODO: handle exception
+                System.out.println("Barbero se fue a dormir");
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
             }
         }
     }
--- src/BarberoDormilon/Main.java
+++ src/BarberoDormilon/Main.java
@@ -2,6 +2,24 @@ package BarberoDormilon;
 
 public class Main {
     public static void main(String[] args) {
+        Barberia elJoni = new Barberia();
+        Barbero joni = new Barbero(elJoni);
+        Cliente losVatos[] = new Cliente[5];
+        Thread losVatosThread[] = new Thread[5];
 
+        for (int i = 0; i < losVatos.length; i++) {
+            losVatos[i] = new Cliente(elJoni, i + 1);
+        }
+
+        for (int j = 0; j < losVatosThread.length; j++) {
+            losVatosThread[j] = new Thread(losVatos[j]);
+        }
+
+        for (int k = 0; k < losVatosThread.length; k++) {
+            losVatosThread[k].start();
+        }
+
+        Thread barber = new Thread(joni);
+        barber.start();
     }
 }
