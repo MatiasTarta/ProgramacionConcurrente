632a396d942501fc1e7972062ce0f39c8403e0ec
MatiasTarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2024-10-29 11:44:48 -0300

    animacion tren

--- src/TP5/Ej5/Maquinista.java
+++ src/TP5/Ej5/Maquinista.java
@@ -12,21 +12,42 @@ public class Maquinista extends Thread {
             if (miTren.trenLleno()) {
                 try {
                     miTren.hacerRecorrido();
-                    Thread.sleep(5000);
-                    System.out.println("El Tren da una vuelta ");
-                    System.out.println("_________\r\n" + //
-                            " |  _  |    __\r\n" + //
-                            " | | | |____\\/_\r\n" + //
-                            " | |_| |       \\\r\n" + //
-                            " | __  |  _  _  |\r\n" + //
-                            " |/  \\_|_/ \\/ \\/\r\n" + //
-                            "  \\__/   \\_/\\_/");
+                    System.out.println("El Tren da una vuelta");
+
+                    int duracionTotalMs = 2000; // Duración total en milisegundos
+                    int desplazamientos = 20; // Número de desplazamientos
+                    int tiempoPorDesplazamiento = duracionTotalMs / desplazamientos;
+
+                    for (int i = 0; i < desplazamientos; i++) {
+                        // Limpia la consola
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
+
+                        // Añade espacios para el desplazamiento del tren
+                        String espaciado = " ".repeat(i);
+                        System.out.println(espaciado + "_________\r\n" + //
+                                espaciado + " |  _  |    __\r\n" + //
+                                espaciado + " | | | |____\\/_\r\n" + //
+                                espaciado + " | |_| |       \\\r\n" + //
+                                espaciado + " | __  |  _  _  |\r\n" + //
+                                espaciado + " |/  \\_|_/ \\/ \\/\r\n" + //
+                                espaciado + "  \\__/   \\_/\\_/");
+
+                        // Pausa para el efecto de animación
+                        Thread.sleep(tiempoPorDesplazamiento);
+                    }
                     miTren.detenerTren();
                     System.out.println("todos abajo");
                     miTren.bajar();
                 } catch (Exception e) {
-                    //
+                    e.printStackTrace();
                 }
+            }
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
         }
     }
